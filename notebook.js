7c400758529d211147714fd30678251af53ed5b2
Adrien Treuille <adrien.g.treuille@gmail.com>
2017-12-28 21:08:05 -0500

    Moving towards a local http server architecture.

---
+++ notebook.js
@@ -0,0 +1,32 @@
+// Autoloads the content of inner-index.html into index.html every 300ms.
+
+// Some magic to let us use ES6 here.
+(function(){
+  'use strict';
+
+  // Run this when the page loads.
+  function main() {
+  };
+
+  // Run this when the page loads.
+  $(document).ready(() => {
+    let innerHtml = ""
+    console.log('Starting load loop.')
+
+    $.ajax('file:///Users/adrien/Desktop/stock-market-simulation/output/nndinner.html')
+    .done((result) => {
+      console.log('Success');
+      console.log(result);
+    })
+    .fail(( xhr, status, errorThrown ) => {
+      console.log( "Sorry, there was a problem!" );
+      console.log( "Error: " + errorThrown );
+      console.log( "Status: " + status );
+      console.dir( xhr );
+    });
+
+    const invk = (v) => v * 2;
+    console.log(invk(1233));
+  });
+
+})();
